0333076f6ad8d891936bbf7f67b9640010ca41ea
codeslw <60443063+codeslw@users.noreply.github.com>
2026-06-01 17:31:26 +0500

    chore: add jest + ts-jest for unit tests

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        module: 'commonjs',
+        esModuleInterop: true,
+        resolveJsonModule: true,
+        types: ['jest', 'node']
+      }
+    }]
+  }
+};
